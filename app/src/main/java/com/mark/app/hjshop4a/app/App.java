666a7f2d78b3cdbffdbb0a5a018b35dc5bbbce5f
panyunpeng <panyunpeng@dlydly.com>
2018-04-12 14:32:30 +0800

    登录注册界面

--- app/src/main/java/com/mark/app/hjshop4a/app/App.java
+++ app/src/main/java/com/mark/app/hjshop4a/app/App.java
@@ -1,10 +1,20 @@
 package com.mark.app.hjshop4a.app;
 
-import android.app.Application;
+import android.support.multidex.MultiDexApplication;
 
 /**
  * Created by zhuwh on 2018/4/10.
  */
 
-public class App extends Application {
+public class App extends MultiDexApplication {
+    private static App mApp;
+
+    /**
+     * 获取Application实例
+     *
+     * @return
+     */
+    public static App getInstance() {
+        return mApp;
+    }
 }
